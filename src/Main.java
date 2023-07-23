0cff7f3ac5177dfb861b08a4506b627c2aea4697
Livenskaya Olga <livenskaya@list.ru>
2023-07-23 16:06:01 +0300

    Change program

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;  // остаток на счету
-        int recharge = 300; //пополнили счет
+        int recharge = 1100; //пополнили счет
         int kofBonus = 100; // коэфициент для расчета бонуса
         System.out.println("На вашем счету было: " + balance + " руб.");
         System.out.println("Вы пополнили счет на: " + recharge + " руб.");
         int bonus;
         if (recharge > 1000) {
             bonus = recharge / kofBonus;
-            ;
             System.out.println("Ваш бонус:" + bonus + " руб.");
             System.out.println("Итоговый остаток на вашем счету: " + (balance + recharge + bonus) + " руб.");
         } else {
